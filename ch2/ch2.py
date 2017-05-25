02654504874fe108e3db726222991239df5f6c26
Satoshi Nagayasu <snaga@uptime.jp>
2017-05-25 17:17:05 +0900

    Add generating a sentense with n-gram and Malkov chain.

--- ch2/ch2.py
+++ ch2/ch2.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import operator
+import random
 import unicodedata
 from collections import OrderedDict
 
@@ -45,10 +46,53 @@ def make_ngram(s, n=3):
         g.append(s[i:i+n])
     return g
 
+def findch(ch, ngram):
+    c = 0
+    for g in ngram:
+        if ch == g[0]:
+            c += 1
+    return c
+
+def setrnd(n):
+    return random.randint(0, n-1)
+
+def setrndstr(ch, ngram):
+    assert len(ch) == 1 and isinstance(ch, unicode)
+    pos = setrnd(len(ngram))
+    return ngram[pos]
+
+def setnext(ch, ngram, num):
+    assert len(ch) == 1 and isinstance(ch, unicode)
+    pos = setrnd(num)
+    i = 0
+    n = None
+    for g in ngram:
+        if ch != g[0]:
+            continue
+        if i == pos:
+            return g
+        i += 1
+    return None
+
+def generate(ch, ngram):
+    s = u''
+    while True:
+        n = findch(ch, ngram)
+        if n > 0:
+            ch2 = setnext(ch, ngram, n)
+        else:
+            ch2 = setrndstr(ch, ngram)
+        s = s + ch2[0]
+        if ch2[0] == u'。' or ch2[0] == u'．':
+            break
+        ch = ch2[1]
+    return s
+
 if __name__ == '__main__':
     s = u"親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張っても、そこから飛び降りる事は出来まい。弱虫やーい。と囃したからである。小使に負ぶさって帰って来た時、おやじが大きな眼をして二階ぐらいから飛び降りて腰を抜かす奴があるかと云ったから、この次は抜かさずに飛んで見せますと答えた。"
     print(s)
 
+    """
     t = getwidechar(s)
     print(t)
     outputtarget(t)
@@ -66,3 +110,20 @@ if __name__ == '__main__':
 
     for t in make2gram(s):
         print(t)
+    """
+
+    _2gram = make2gram(s)
+    """
+    print "--"
+    print findch(u'小', _2gram)
+
+    print setrndstr(u'小', _2gram)
+    print "--"
+    print setnext(u'小', _2gram, findch(u'小', _2gram))
+    """
+
+    random.seed(1)
+    print generate(u'小', _2gram)
+    print generate(u'親', _2gram)
+    print generate(u'一', _2gram)
+    print generate(u'二', _2gram)
